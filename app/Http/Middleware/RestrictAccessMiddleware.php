1550764889e6cabc38cc02be10150c8ff0696f52
llayz46 <layzer@outlook.fr>
2025-04-01 12:39:37 +0200

    debug: RestrictAccessMiddleware.php

--- app/Http/Middleware/RestrictAccessMiddleware.php
+++ app/Http/Middleware/RestrictAccessMiddleware.php
@@ -10,13 +10,16 @@ class RestrictAccessMiddleware
     public function handle(Request $request, Closure $next)
     {
         $authorizedIp = env('AUTHORIZED_IP_ACCESS');
+        $requestIp = $request->getClientIp();
 
         if (empty($authorizedIp)) {
             abort(500, 'La variable d\'environnement AUTHORIZED_IP_ACCESS n\'est pas définie.');
         }
 
-        if ($request->ip() !== $authorizedIp) {
-            abort(403, 'Accès non autorisé.');
+        if ($requestIp !== $authorizedIp) {
+            abort(403, 'Accès non autorisé.' .
+                ' Votre adresse IP est ' . $requestIp .
+                ' et l\'adresse IP autorisée est ' . $authorizedIp);
         }
 
         return $next($request);
